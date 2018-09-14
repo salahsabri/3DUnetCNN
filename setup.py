6fcec573abd370d3f657d103a00b354907c3961f
salahsabri <41323692+salahsabri@users.noreply.github.com>
2018-09-14 16:35:52 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='xlearn',
+    author='Salah',
+    packages=find_packages(),
+    version=open('VERSION').read().strip(),
+    description = 'Keras 3D U-Net Convolution Neural Network (CNN) designed for medical image segmentation',
+    license='BSD-3',
+    platforms='Any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: BSD-3',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
